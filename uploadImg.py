34d81e0fffa2c00e2b08121a501f27b7e13c1d8a
wanlf <azathothes@outlook.com>
2016-04-07 17:15:36 +0800

    fill the declear

--- uploadImg.py
+++ uploadImg.py
@@ -2,15 +2,18 @@ import imgSave
 from imgSave import Mongo
 import bson
 from cStringIO import StringIO
-
+#import the imgSave module , the class Mongo is the one who runs database , the class's ctor requires four arguments
+#stand for the database's ip , port , database you need to connect and the collection you need to reach.
+#in the origin edition i just write it dead in the code. and we will save it in a config file in the furure.
 mongo = Mongo("127.0.0.1",27017,"img","img");
 
 if mongo.OpenConn():
     print "conn successful!"
 else:
     print "conn error!"
-
-
+#so this is the function that client invoke to upload images into mongodb.
+#the arguments the function take is the longitude and the latitude the image stand for
+#, x , y , z ,and the img_path is image's path in your computer.
 def UploadImg(x,y,z,img_path):
     try:
         img = open(img_path,'rb');

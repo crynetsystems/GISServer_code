08586beb2705d450d85c839ff3944cc3cd2c54d3
wanlf <azathothes@outlook.com>
2016-04-16 18:03:51 +0800

    merge

--- arcgisdownloader.py
+++ arcgisdownloader.py
@@ -6,12 +6,18 @@ import os
 import urllib2
 import multiprocessing.dummy
 from time import sleep
-maxthreads = 500;
-maxlinksize = 2000;
-dir_path='/var/usb/download_tiles/'
-test_id=0
-thread_num=0
-max_downloadFrequency = 20
+from conn import Mongo;
+maxthreads = 900;
+maxlinksize = 5000;
+dir_path="G:/test01/"
+
+mongo = Mongo('127.0.0.1',27017,'errorInfos','errorInfo');
+if mongo.OpenConn() != True:
+    print "connect initialize failed."
+linkQueue = multiprocessing.dummy.Queue(maxlinksize);
+errorQueue = multiprocessing.dummy.Queue();
+
+
 
 def num2deg(xtile, ytile, zoom):
     n = 2.0 ** zoom
@@ -26,7 +32,6 @@ def deg2num(lat_deg, lon_deg, zoom):
     xtile = int((lon_deg + 180.0) / 360.0 * n)
     ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
     return (xtile, ytile)
-
 
 def lla2url(lat,lon,zoom):
     (x,y)=deg2num(lat,lon,zoom)
@@ -51,8 +56,6 @@ def xyz2path(x,y,zoom):
     return dir_path+s_zoom+s_x+s_y+'.jpg'
 
 def DownloadImage(image_url,path):
-    if os.path.isfile(path):
-        return 1
     try:
         f=urllib2.urlopen(image_url,timeout = 5).read()
     except:
@@ -65,18 +68,13 @@ def DownloadImage(image_url,path):
 def DownloadTile(zoom,x,y,count):
         sourceurl=xyz2url(x,y,zoom)
         path=xyz2path(x,y,zoom)
-        retry_time=max_downloadFrequency
-        while (DownloadImage(sourceurl,path)==-1):
-            retry_time-=1
-            print zoom,x,y,'redownload',max_downloadFrequency-retry_time
-            if retry_time==0:
-                print zoom,x,y,'redownload'
-                return 0
-
-
+        if (DownloadImage(sourceurl,path)==-1):
+            return 0;
+        else:
+            return 1;
 
 def InsertURLInfo(linkQueue):
-    for zoom in range(0,13):
+    for zoom in range(0,7):
         this_xy=2**(zoom)
         for x in range(0,this_xy):
             try:
@@ -86,43 +84,44 @@ def InsertURLInfo(linkQueue):
             except:
                 pass
             for y in range(0,this_xy):
+                
                 isWaiting = 0;
                 while isWaiting == 0:
                     try:
                         linkQueue.put_nowait(dict(x=x,y=y,z=zoom,count=0))
                         isWaiting = 1;
                     except:
-                        sleep(0.1);
+                        sleep(1);
 
-
-def downLoadImg(linkQueue):
-    current_Quene = linkQueue
-    while current_Quene.qsize() > 0 :
-        isWaiting = 0;
-        sleep_time=100
-        while isWaiting == 0:
+def downLoadImg(linkQueue,errorQueue):
+    current_Quene = linkQueue;
+    while linkQueue.qsize() > 0 or errorQueue.qsize() > 0 :
+        if errorQueue.qsize() >= 10000 or (linkQueue.qsize() == 0 and errorQueue.qsize > 0):
+            current_Quene = errorQueue;
+        isGet = 0;
+        while isGet == 0:
             try:
                 urlInfo = current_Quene.get_nowait();
-                if urlInfo['count'] >= max_downloadFrequency:
-                    print "oh,just download fall..."
+                if urlInfo['count'] == 8:
+                    print "save bad errors."
+                    mongo.SaveURLInfo(urlInfo);
                     continue;
-                DownloadTile(urlInfo['z'],urlInfo['x'],urlInfo['y'],urlInfo['count']);
-                isWaiting = 1;
+                if DownloadTile(urlInfo['z'],urlInfo['x'],urlInfo['y'],urlInfo['count']) == 0:
+                    print "error Queue size:" + str(errorQueue.qsize());
+                    errorQueue.put_nowait(dict(x=urlInfo['x'],y=urlInfo['y'],z=urlInfo['z'],count=urlInfo['count']+1));
+                isGet = 1;
             except:
-                sleep_time=sleep_time-1
-                if sleep_time == 0:
-                    isWaiting=1
-                sleep(0.01);
+                sleep(1);
+
+
 
 if __name__ == '__main__':
-    linkQueue = multiprocessing.dummy.Queue(maxlinksize);
-    errorQueue = multiprocessing.dummy.Queue();
     cacheURL = multiprocessing.dummy.Process(target = InsertURLInfo,args = (linkQueue,))
     cacheURL.start();
-    sleep(1);
-
+    sleep(10);
+    print "begin download."
     for i in range(0,maxthreads):
-        download = multiprocessing.dummy.Process(target = downLoadImg,args = (linkQueue,))
+        download = multiprocessing.dummy.Process(target = downLoadImg,args = (linkQueue,errorQueue))
         download.start();
-        sleep(0.001)
-    print "start!!!!~~~"
+        sleep(0.01);
+    print "finish."
